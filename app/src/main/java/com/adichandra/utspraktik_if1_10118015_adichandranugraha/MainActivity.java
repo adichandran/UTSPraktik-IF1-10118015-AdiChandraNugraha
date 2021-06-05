a722f546541d916e7cdccfbf3b14681a6288760b
adichnadra <adichandra019@gmail.com>
2021-06-05 13:37:21 +0700

    inisiasi project

--- app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity.java
+++ app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity.java
@@ -2,20 +2,87 @@ package com.adichandra.utspraktik_if1_10118015_adichandranugraha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtnik, edtnama, edtdate;
+    Button selanjutnya;
+    SharedPreferences preferences;
+    SimpleDateFormat dateFormetter;
+    RadioGroup rg;
+
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NIK = "nik";
+    private static final String KEY_NAMA = "nama";
+    private static final String KEY_TTL = "ttl";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtnik = findViewById(R.id.edtnik);
+        edtnama = findViewById(R.id.edtnama);
+        edtdate = findViewById(R.id.edtDate);
+        dateFormetter = new SimpleDateFormat("dd MM yyyy");
+        selanjutnya = findViewById(R.id.selanjutnya);
+
+        preferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+
+        edtdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showdatedialog();
+            }
+        });
+
+
+        selanjutnya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(KEY_NIK,edtnik.getText().toString());
+                editor.putString(KEY_NAMA,edtnama.getText().toString());
+                editor.putString(KEY_TTL,edtnama.getText().toString());
+                editor.apply();
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void btnselanjutnya(View view) {
-        startActivity(new Intent(MainActivity.this, MainActivity2.class));
-        finish();
+    private void showdatedialog() {
+        Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newdate = Calendar.getInstance();
+                newdate.set(year, month, dayOfMonth);
+                edtdate.setText(dateFormetter.format(newdate.getTime()));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+
     }
+
+
 }
--- app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity2.java
+++ app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity2.java
@@ -3,21 +3,51 @@ package com.adichandra.utspraktik_if1_10118015_adichandranugraha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    EditText edtnik, edtnama, edtdate, edtkel;
+    SharedPreferences preferences;
+
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_NIK = "nik";
+    private static final String KEY_NAMA = "nama";
+    private static final String KEY_TTL = "ttl";
+    private static final String KEY_KEL = "kelamin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        edtnik = findViewById(R.id.edtnik);
+        edtnama = findViewById(R.id.edtnama);
+        edtdate = findViewById(R.id.edtTtl);
+        edtkel = findViewById(R.id.editTextkel);
+
+        preferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+        String nik = preferences.getString(KEY_NIK,null);
+        String nama = preferences.getString(KEY_NAMA,null);
+        String ttl = preferences.getString(KEY_TTL,null);
+        String kel = preferences.getString(KEY_KEL,null);
+
+        edtnik.setText(nik);
+        edtnama.setText(nama);
+        edtdate.setText(ttl);
+        edtkel.setText(ttl);
+
         Dialog dialog;
         Button simpan = findViewById(R.id.simpan);
+        Button ubah = findViewById(R.id.ubah);
 
         dialog = new Dialog(this);
 
@@ -30,6 +60,17 @@ public class MainActivity2 extends AppCompatActivity {
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
 
+
+
+            }
+        });
+
+        ubah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
             }
         });
     }
