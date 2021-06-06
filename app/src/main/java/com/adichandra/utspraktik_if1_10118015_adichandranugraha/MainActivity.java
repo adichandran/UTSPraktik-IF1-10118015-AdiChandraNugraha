8ee35736badda8fb170b7deaf8d5941301c807f1
adichnadra <adichandra019@gmail.com>
2021-06-06 10:35:37 +0700

    inisiasi project

--- app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity.java
+++ app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity.java
@@ -1,6 +1,7 @@
 package com.adichandra.utspraktik_if1_10118015_adichandranugraha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
@@ -25,12 +26,15 @@ public class MainActivity extends AppCompatActivity {
     Button selanjutnya;
     SharedPreferences preferences;
     SimpleDateFormat dateFormetter;
-    RadioGroup rg;
+    RadioGroup kel, hub;
+    RadioButton rkel, rhub;
 
     private static final String SHARED_PREF_NAME = "mypref";
     private static final String KEY_NIK = "nik";
     private static final String KEY_NAMA = "nama";
     private static final String KEY_TTL = "ttl";
+    private static final String KEY_KEL = "Kel";
+    private static final String KEY_HUB = "hub";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         edtnik = findViewById(R.id.edtnik);
         edtnama = findViewById(R.id.edtnama);
         edtdate = findViewById(R.id.edtDate);
-        dateFormetter = new SimpleDateFormat("dd MM yyyy");
+        kel = findViewById(R.id.jeniskelamin);
+        hub = findViewById(R.id.hubungan);
+
+        dateFormetter = new SimpleDateFormat("dd MMMM yyyy");
         selanjutnya = findViewById(R.id.selanjutnya);
 
         preferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
@@ -48,7 +55,17 @@ public class MainActivity extends AppCompatActivity {
         edtdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showdatedialog();
+                Calendar calendar = Calendar.getInstance();
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar newdate = Calendar.getInstance();
+                        newdate.set(year, month, dayOfMonth);
+                        edtdate.setText(dateFormetter.format(newdate.getTime()));
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
             }
         });
 
@@ -56,10 +73,14 @@ public class MainActivity extends AppCompatActivity {
         selanjutnya.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getvalueradio();
+
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putString(KEY_NIK,edtnik.getText().toString());
                 editor.putString(KEY_NAMA,edtnama.getText().toString());
-                editor.putString(KEY_TTL,edtnama.getText().toString());
+                editor.putString(KEY_TTL,edtdate.getText().toString());
+                editor.putString(KEY_KEL,rkel.getText().toString());
+                editor.putString(KEY_HUB,rhub.getText().toString());
                 editor.apply();
 
                 Intent intent = new Intent(MainActivity.this,MainActivity2.class);
@@ -69,20 +90,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void showdatedialog() {
-        Calendar calendar = Calendar.getInstance();
+    private void getvalueradio() {
+        // get selected radio button from radioGroup
+        int selectedId = kel.getCheckedRadioButtonId();
+        int selectedId2 = hub.getCheckedRadioButtonId();
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                Calendar newdate = Calendar.getInstance();
-                newdate.set(year, month, dayOfMonth);
-                edtdate.setText(dateFormetter.format(newdate.getTime()));
-            }
-        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        datePickerDialog.show();
-
+        // find the radiobutton by returned id
+        rkel = (RadioButton) findViewById(selectedId);
+        rhub = (RadioButton) findViewById(selectedId2);
     }
-
-
 }
--- app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity2.java
+++ app/src/main/java/com/adichandra/utspraktik_if1_10118015_adichandranugraha/MainActivity2.java
@@ -15,14 +15,15 @@ import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText edtnik, edtnama, edtdate, edtkel;
+    EditText edtnik, edtnama, edtdate, edtkel, edthub;
     SharedPreferences preferences;
 
     private static final String SHARED_PREF_NAME = "mypref";
     private static final String KEY_NIK = "nik";
     private static final String KEY_NAMA = "nama";
     private static final String KEY_TTL = "ttl";
-    private static final String KEY_KEL = "kelamin";
+    private static final String KEY_KEL = "Kel";
+    private static final String KEY_HUB = "hub";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +34,20 @@ public class MainActivity2 extends AppCompatActivity {
         edtnama = findViewById(R.id.edtnama);
         edtdate = findViewById(R.id.edtTtl);
         edtkel = findViewById(R.id.editTextkel);
+        edthub = findViewById(R.id.editTexthub);
 
         preferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
         String nik = preferences.getString(KEY_NIK,null);
         String nama = preferences.getString(KEY_NAMA,null);
         String ttl = preferences.getString(KEY_TTL,null);
         String kel = preferences.getString(KEY_KEL,null);
+        String hub = preferences.getString(KEY_HUB,null);
 
         edtnik.setText(nik);
         edtnama.setText(nama);
         edtdate.setText(ttl);
-        edtkel.setText(ttl);
+        edtkel.setText(kel);
+        edthub.setText(hub);
 
         Dialog dialog;
         Button simpan = findViewById(R.id.simpan);
@@ -59,18 +63,19 @@ public class MainActivity2 extends AppCompatActivity {
 
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
-
-
-
             }
         });
 
         ubah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
+    }
+
+    public void btnselanjutnya(View view) {
+        Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(intent);
     }
 }
